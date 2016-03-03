1e5336154406f7d6fde1d1a6cc2583d0370c0ba0
Ruben Carranza <carranzaruben2vd@hotmail.com>
2016-03-03 16:12:49 -0600

    Practica Runif
    clase

---
+++ actividad runif.R
@@ -0,0 +1,13 @@
+x <- runif(1,0,100)
+if (x<=30){
+    y="Hola"
+} else if (x>30 && x<=50){
+    y="Que onda"
+} else if (x>50 && x<=60){
+    y="Que pedo wey"
+} else if (x>60 && x<=80){
+    y="Que pex"
+} else if (x>80 && x<=100){
+    y="quihubo"
+}
+y
