421e0accb89bc3b90420140d92487387c4c0887d
Ruben Carranza <carranzaruben2vd@hotmail.com>
2016-03-04 15:31:17 -0600

    Prueba de funcionamiento
    Actividad con "for"

--- actividad runif.R
+++ actividad runif.R
@@ -11,3 +11,57 @@ if (x<=30){
     y="quihubo"
 }
 y
+
+# Ejercicio If nacho
+x <- runif(1)
+if(x<=0.3){
+    print("Hola")
+} else if (x<=0.5){
+    print("¿Que onda?")
+} else if (x<=0.7){
+    print("Quiubo")
+} else if (x<=0.9){
+    print("Que pex")
+} else {
+    print("Que pedo wey")
+}  
+
+
+
+# Ejemplo For
+for(i in 10:1){
+    print(i)
+}
+
+x <- matrix(1:6,2,3,T)
+x
+for(i in seq_len(nrow(x))){
+    for(j in seq_len(ncol(x))){
+        print(x[i,j])
+    }
+}
+
+
+# Prueba de funcionamiento
+n <- 10000
+mensajes <- vector("character",n)
+for(i in 1:n){
+    y <- runif(1)
+    if(y<=0.30){
+        mensajes[i]<-"Mensaje 30%"
+    } else if (y<=0.50){
+        mensajes[i]<-"Mensaje 20% a"
+    } else if (y<= 0.7){
+        mensajes[i]<-"Mensaje 20% b"
+    } else if (y<=.9){
+        mensajes[i]<-"Mensaje 20% c"
+    } else{
+        mensajes[i]<-"Mensaje 10%"
+    } 
+}
+factor(mensajes)
+table(mensajes)
+plot(table(mensajes))
+
+
+
